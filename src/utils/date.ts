c9a3c44ad6f2be2b0349c7d92765383b3e38c2ff
الاهرام للتجارة والتوزيع <y01009025501@gmail.com>
2026-05-09 14:46:08 +0300

    Delete src directory

--- src/utils/date.ts
+++
@@ -1,5 +0,0 @@
-export const formatDateTime = (value: string): string =>
-  new Intl.DateTimeFormat('ar-EG', {
-    dateStyle: 'medium',
-    timeStyle: 'short',
-  }).format(new Date(value));
--- src/utils/money.ts
+++
@@ -1,6 +0,0 @@
-export const money = (value: number, currency = 'EGP'): string =>
-  new Intl.NumberFormat('ar-EG', {
-    style: 'currency',
-    currency,
-    maximumFractionDigits: 0,
-  }).format(value);
