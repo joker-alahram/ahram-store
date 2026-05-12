8033372a2e5d4895783e0603fbbaf64725619a39
الاهرام للتجارة والتوزيع <y01009025501@gmail.com>
2026-05-12 15:49:07 +0300

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-import { bootstrapApp } from './src/runtime/bootstrap.js';
-
-bootstrapApp();
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('./sw.js').catch(() => {});
-  });
-}
