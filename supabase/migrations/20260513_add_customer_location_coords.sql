4d0681792752d97e4d9e2a060b91d123b490b856
الاهرام للتجارة والتوزيع <y01009025501@gmail.com>
2026-05-14 15:58:04 +0300

    Delete supabase/migrations directory

--- supabase/migrations/20260513_add_customer_location_coords.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE public.customers
-  ADD COLUMN IF NOT EXISTS location_lat numeric,
-  ADD COLUMN IF NOT EXISTS location_lng numeric;
-
-COMMENT ON COLUMN public.customers.location_lat IS 'Latitude captured at customer creation';
-COMMENT ON COLUMN public.customers.location_lng IS 'Longitude captured at customer creation';
